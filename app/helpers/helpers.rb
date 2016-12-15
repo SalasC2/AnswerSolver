c3b3d848b9cfe37864e474334eb9646dc2293d54
Meriç Bal <email": null>
2016-12-15 12:40:38 -0800

    Added Sessions and helper methods.

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,20 @@
+helpers do
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout
+    session.clear
+    # or session.delete(:user_id)
+  end
+
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      nil
+    end
+  end
+
+end
