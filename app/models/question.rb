28b7030da26818b265c78a6d1e6430c5fe91251f
SalasC2 <christiansalas96@gmail.com>
2016-12-15 15:50:44 -0800

    Add Views for questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :user
   has_many :answers
-  has_many :comments as: :commentable
-  has_many :votes as: :voteable
+  has_many :comments, as: :commentable
+  has_many :votes, as: :voteable
 end
