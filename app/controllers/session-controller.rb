ed78b9565b4a599a6ad3c79835e5eb1a4597d6a1
Meriç Bal <email": null>
2016-12-15 12:58:13 -0800

    Added User Auth , simple login form , session controller, Bcrypt, etc etc ..

---
+++ app/controllers/session-controller.rb
@@ -0,0 +1,22 @@
+get '/login' do
+  erb :'/sessions/new'
+end
+
+post '/login' do
+  @user = User.unlock(params[:user][:username], params[:user][:password])
+
+  if @user && @user.password == params[:user][:password]
+    login(@user)
+    # route
+  else
+    status 422
+    @login_error = 'Username and Password does NOT match !'
+    erb :'/sessions/new'
+  end
+
+end
+
+delete '/logout' do
+  logout
+  # route
+end
