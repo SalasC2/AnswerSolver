06168362e8d395ba82b44babd5645e10a2fcc9f0
Katee Trant <katee.trant@gmail.com>
2016-12-15 21:15:07 -0800

    Change current_user helper method and add logged_in? helper method

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -10,12 +10,19 @@ helpers do
     # session.delete(:user_id)
   end
 
-  def current_user
-    if session[:user_id]
-      User.find(session[:user_id])
-    else
-      nil
-    end
-  end
+  # def current_user
+  #   if session[:user_id]
+  #     User.find(session[:user_id])
+  #   else
+  #     nil
+  #   end
+  # end
+ def current_user
+   @current_user ||= User.find(session[:id]) if session[:id]
+ end
 
+ def logged_in?
+   current_user
+ end
+ 
 end
